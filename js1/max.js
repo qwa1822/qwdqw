e6443be70c1deccf54ff0424921a0e01abd32754
qwa1822 <asdzxc1822@naver.com>
2023-02-08 15:05:25 +0900

    Math

---
+++ js1/max.js
@@ -0,0 +1,27 @@
+console.log(Math.max(1,-1));
+console.log(Math.min(1,-1));
+
+console.log(Math.max(1,-1,5,23,17,-4));
+console.log(Math.max(1,-1,5,23,17,-4));
+
+
+let nums=[1,-1,5,23,17,-4]
+console.log(Math.max.apply(null,nums));
+console.log(Math.min.apply(null,nums));
+
+// spread
+
+console.log(Math.max(...nums));
+console.log(Math.min(...nums));
+
+
+console.log(Math.abs(1));
+console.log(Math.abs(-1));
+console.log(Math.abs(-Infinity));
+
+
+
+for(let i=0; i<10; i++){
+  console.log(Number.parseInt(Math.random()*1000+1));
+}
+
